8a62e50ec09df77f10670efca4b44e088bbb372e
simulasikode <simulasikode@gmail.com>
2024-11-02 21:24:24 +0700

    edit: Layout Landing page

--- src/app/components/Foreword/Index.jsx
+++ src/app/components/Foreword/Index.jsx
@@ -3,7 +3,15 @@ import styles from "./foreword.module.scss";
 export default function Landing() {
   return (
     <div className={styles.foreword}>
-      <h1>Pengantar</h1>
+      <p>Festival Seni Cetak Grafis “Trilogia”: Seni Cetak yang Bergerak </p>
+      <h2>
+        Merupakan upaya Krack sebagai kelompok pegiat seni cetak grafis untuk
+        merayakan praktik dan pengetahuan yang senantiasa berkembang luas dan
+        terepresentasi dalam seni cetak grafis. Watak seni cetak grafis yang
+        masal, distributif, repetitif dan kontekstual akan kami jadikan pintu
+        masuk untuk membicarakan berbagai topik dalam dinamika sosial budaya
+        masyarakat.
+      </h2>
     </div>
   );
 }
--- src/app/components/Landing/Index.jsx
+++ src/app/components/Landing/Index.jsx
@@ -3,7 +3,8 @@ import styles from "./landing.module.scss";
 export default function Landing() {
   return (
     <div className={styles.landing}>
-      <h1>Landing page</h1>
+      <p>Festival Seni Cetak Grafis: Trilogia</p>
+      <h1>Seni Cetak Grafis yang Bergerak</h1>
     </div>
   );
 }
